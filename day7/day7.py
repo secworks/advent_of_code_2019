611aebf73ed5abaeabd6ec75c81f0fa09a49f661
Joachim Strömbergson <joachim@secworks.se>
2019-12-07 08:50:52 +0100

    Adding test cases for all examplex.

--- day7/day7.py
+++ day7/day7.py
@@ -174,12 +174,30 @@ def get_signal(prog, phases):
 # Provide 1 as input.
 #-------------------------------------------------------------------
 def problem1():
+    TEST1 = True
+
     print("Problem 1")
     test_prog1_1 = [3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0]
     test_phase1_1 = [4,3,2,1,0]
 
-    signal = get_signal(test_prog1_1, test_phase1_1)
-    print("Thrust signal from running test 1: %d" % signal)
+    test_prog1_2 = [3,23,3,24,1002,24,10,24,1002,23,-1,23,
+                    101,5,23,23,1,24,23,23,4,23,99,0,0]
+    test_phase1_2 = [0,1,2,3,4]
+
+    test_prog1_3 = [3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,
+                    31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,
+                    4,31,99,0,0,0]
+    test_phase1_3 = [1,0,4,3,2]
+
+    if (TEST1):
+        signal = get_signal(test_prog1_1, test_phase1_1)
+        print("Thrust signal from running test 1: %d" % signal)
+
+        signal = get_signal(test_prog1_2, test_phase1_2)
+        print("Thrust signal from running test 2: %d" % signal)
+
+        signal = get_signal(test_prog1_3, test_phase1_3)
+        print("Thrust signal from running test 3: %d" % signal)
 
 
 #-------------------------------------------------------------------
