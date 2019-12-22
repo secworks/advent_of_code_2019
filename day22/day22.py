506a19ce1fec80425482acc88061c5b799ccee44
Joachim Strömbergson <joachim@secworks.se>
2019-12-22 20:10:42 +0100

    Completed the shuffling instructions.

--- day22/day22.py
+++ day22/day22.py
@@ -22,37 +22,40 @@ def find_card(deck, card):
 
 #-------------------------------------------------------------------
 #-------------------------------------------------------------------
-def reverse(stack):
-    return stack
-
-
-#-------------------------------------------------------------------
-#-------------------------------------------------------------------
-def increment(stack, inc):
-    return stack
-
-
-#-------------------------------------------------------------------
-#-------------------------------------------------------------------
-def cut(stack, pos):
-    return stack
+def increment(deck, inc):
+    orig_len = len(deck)
+    tmp = [0] * orig_len
+    ptr = 0
+    while len(deck) > 0:
+        e = deck.pop(0)
+        tmp[ptr] = e
+        ptr = (ptr + inc) % orig_len
+    return tmp
 
 
 #-------------------------------------------------------------------
 #-------------------------------------------------------------------
 def shuffle(deck, instructions):
     for i in instructions:
+        print(deck)
         if "deal into new stack" in i:
-            stack = reverse(stack)
+            print("Reversing")
+            deck.reverse()
 
-        if "deal with increment" in i:
-            inc = int(i.split(" ")[3])
-            stack = increment(stack, inc)
 
-        if "cut" in i:
+        elif "cut" in i:
             pos = int(i.split(" ")[1])
-            stack = cut(stack, pos)
+            print("Doing cut at pos %d" % pos)
+            deck = deck[pos:] + deck[:pos]
 
+
+        elif "deal with increment" in i:
+            inc = int(i.split(" ")[3])
+            print("Deal with increment at %d" % inc)
+            deck = increment(deck, inc)
+
+        else:
+            print("Unrecognized command: %s" % i)
     return deck
 
 
@@ -61,6 +64,27 @@ def shuffle(deck, instructions):
 #-------------------------------------------------------------------
 def problem1():
     print("Problem 1")
+    test_deck1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    test_instr1 = ["deal with increment 7", "deal into new stack",
+                   "deal into new stack"]
+
+    test_instr2 = ["cut 6", "deal with increment 7",
+                   "deal into new stack"]
+
+    test_instr3 = ["deal with increment 7", "deal with increment 9",
+                   "cut -2"]
+
+    test_instr4 = ["deal into new stack", "cut -2",
+                   "deal with increment 7", "cut 8",
+                   "cut -4", "deal with increment 7", "cut 3",
+                   "deal with increment 9", "deal with increment 3",
+                   "cut -1"]
+
+#    assert(shuffle(test_deck1[:], test_instr1) == [0,3,6,9,2,5,8,1,4,7])
+#    assert(shuffle(test_deck1[:], test_instr2) == [3,0,7,4,1,8,5,2,9,6])
+#    assert(shuffle(test_deck1[:], test_instr3) == [6,3,0,7,4,1,8,5,2,9])
+#    assert(shuffle(test_deck1[:], test_instr4) == [9,2,5,8,1,4,7,0,3,6])
     print("")
 
 #-------------------------------------------------------------------
