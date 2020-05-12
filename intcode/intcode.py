5d00c4cc99093bd76ad353043d1c83925c9881de
Joachim Strömbergson <joachim@assured.se>
2020-05-12 17:02:03 +0200

    Completed the Intcode engine enough to run day2 problems.

--- intcode/intcode.py
+++ intcode/intcode.py
@@ -49,6 +49,7 @@ class Intcode():
             else:
                 print("Continue to execute program in memory.")
 
+
         while(self.state != "halt"):
             opcode = self.mem[self.ip]
             param1 = self.mem[self.ip + 1]
@@ -83,16 +84,46 @@ class Intcode():
                 return (self.state, opcode)
 
 
+#-------------------------------------------------------------------
+#-------------------------------------------------------------------
+def run_day2_1():
+    program = [1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,13,1,19,1,6,19,23,2,6,23,27,1,5,27,31,2,31,9,35,1,35,5,39,1,39,5,43,1,43,10,47,2,6,47,51,1,51,5,55,2,55,6,59,1,5,59,63,2,63,6,67,1,5,67,71,1,71,6,75,2,75,10,79,1,79,5,83,2,83,6,87,1,87,5,91,2,9,91,95,1,95,6,99,2,9,99,103,2,9,103,107,1,5,107,111,1,111,5,115,1,115,13,119,1,13,119,123,2,6,123,127,1,5,127,131,1,9,131,135,1,135,9,139,2,139,6,143,1,143,5,147,2,147,6,151,1,5,151,155,2,6,155,159,1,159,2,163,1,9,163,0,99,2,0,14,0]
+    program[1] = 12
+    program[2] = 2
+
+    print("Running program for day2, problem 1.")
+    my_intcode = Intcode(len(program), debug=False)
+    my_response = my_intcode.load(program)
+    (response, value) = my_intcode.run()
+    if (my_intcode.mem[0] ==  10566835):
+        print("Correct answer for day2 problem 1 received.")
+
+
+#-------------------------------------------------------------------
+#-------------------------------------------------------------------
+def run_test(prog, expected):
+    my_intcode = Intcode(16)
+    my_response = my_intcode.load(prog)
+    (response, value) = my_intcode.run()
+    if (my_response == "unknown"):
+        print("Unknown instruction executed")
+    else:
+        if (my_intcode.mem[0 : len(prog)] == expected):
+            print("Correct result received.")
+        else:
+            print("Incorrect result received.")
+            print("Exp:", expected)
+            print("Got:", my_intcode.mem[0 : len(prog)])
+    print("")
 
 #-------------------------------------------------------------------
 #-------------------------------------------------------------------
 def selftest():
-    my_intcode = Intcode(10)
-    my_response = my_intcode.load([1,0,0,0,99])
-    my_response = my_intcode.run()
-    print(my_response)
-    print(my_intcode.mem)
-
+    run_test([1,0,0,0,99],          [2,0,0,0,99])
+    run_test([2,3,0,3,99],          [2,3,0,6,99])
+    run_test([2,4,4,5,99,0],        [2,4,4,5,99,9801])
+    run_test([1,1,1,4,99,5,6,0,99], [30,1,1,4,2,5,6,0,99])
+    run_day2_1()
 
 #-------------------------------------------------------------------
 #-------------------------------------------------------------------
