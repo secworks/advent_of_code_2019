914290302da0f316e422fa74d6aece90bd069895
Joachim Strömbergson <joachim@assured.se>
2020-05-23 15:36:56 +0200

    Adding som initial attempts at creating the needed functionality

--- day10/day10.py
+++ day10/day10.py
@@ -7,10 +7,30 @@
 #-------------------------------------------------------------------
 def get_input(filename):
     l = []
-    with open(filename,'r') as f:
+    with open("day10_input.txt",'r') as f:
         for line in f:
             l.append(line.strip())
     return l
+
+
+#-------------------------------------------------------------------
+# Find coodinates for all asteroids. Not sure if this is needed.
+#-------------------------------------------------------------------
+def find_all_asteroids(board):
+    columns = len(board)
+    rows = len(board[0])
+    print("Field width and height: (%d, %d)" % (columns, rows))
+    print("Total positions: %d" % (columns * rows))
+    print("")
+
+    asteroids = []
+    for r in range(rows):
+        curr_row = board[r]
+        for c in range(columns):
+            if curr_row[c] == "#":
+                asteroids.append((c, r))
+                print("Asteroid at (%d, %d)" % (c, r))
+    return asteroids
 
 
 #-------------------------------------------------------------------
@@ -19,9 +39,16 @@ def problem1():
     print("Problem 1")
 
     my_map = get_input("day10_input.txt")
-    for line in my_map:
-        print(line)
+    print("My map:")
+    print(my_map)
     print("")
+
+    my_asteroids = find_all_asteroids(my_map)
+    print("Total number of asteroids: %d" % (len(my_asteroids)))
+    print("My asteroids are at:")
+    print(my_asteroids)
+    print("")
+
 
 #-------------------------------------------------------------------
 #-------------------------------------------------------------------
@@ -32,6 +59,6 @@ def problem2():
 #-------------------------------------------------------------------
 #-------------------------------------------------------------------
 problem1()
-problem2()
+#problem2()
 
 #=======================================================================
